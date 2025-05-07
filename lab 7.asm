b18c88e93eda7c01fed53807310213efbeedb307
ashik-billah <ashik.210101@s.pust.ac.bd>
2025-05-07 08:35:07 +0600

    Print A to Z with message

---
+++ lab 7.asm
@@ -0,0 +1,39 @@
+.model small
+.stack 100h
+.data
+a db 'Loop Concept $'
+b db 'a'
+.code
+main proc
+    mov ax, @data
+    mov ds,ax
+    
+    mov ah,9
+    lea dx,a
+    int 21h
+    
+    mov cx,26  
+    
+    loop1: 
+    mov ah,2
+    mov dl,b
+    int 21h
+    inc b  
+    
+    mov ah,2
+    mov dl,' '
+    int 21h
+    
+    mov ah,2
+    mov dl,10
+    int 21h
+    mov dl,13
+    int 21h
+    
+    loop loop1
+    
+    exit:
+    mov ah,4ch
+    int 21h
+    main endp
+end main
